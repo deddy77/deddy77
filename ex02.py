1feadf5ec5381aac296dc4a02e05380a2e031360
Wood Eddy Donald Jean Julien <wjeanjulien@gmail.com>
2023-08-23 23:15:07 -0400

    sum

---
+++ ex02.py
@@ -0,0 +1,16 @@
+nzt = input("Enter your name")
+print('Hello', nzt)
+
+# exercice 2.3
+hours = input("Enter Hours:")
+rate = input("Enter Rate:")
+pay = float(hours) * float(rate)
+print("Pay:", pay)
+
+# exercice 2.4
+width = 17
+height = 12.0
+print(width//2)
+print(width/2.0)
+
+
